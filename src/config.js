fc00050a7690851098cdbd106aa7ef5d8dc6d5f1
Cristian Isaac Vargas <65677401+crisvargas03@users.noreply.github.com>
2023-06-08 17:58:39 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,5 +4,4 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 export const DATABASE_URL =
-    process.env.DATABASE_URL ||
-    'mysql://kjg8uzbr1do0d3j9w3ve:pscale_pw_nS7oL8eSPPyRqB6BE05ELnGxte5YlzD7yHWbZ8qV8fn@aws.connect.psdb.cloud/max-api-db?ssl={"rejectUnauthorized":true}';
+    process.env.DATABASE_URL;
